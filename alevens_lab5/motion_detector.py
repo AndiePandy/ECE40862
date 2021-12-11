8b5a8add2e1685a53187a99e184471c5bb8f09e5
AndiePandy <72839407+AndiePandy@users.noreply.github.com>
2021-12-10 22:18:40 -0500

    Update motion_detector.py
    just final testing left
    will update if there are any fixes

--- alevens_lab5/motion_detector.py
+++ alevens_lab5/motion_detector.py
@@ -1,6 +1,6 @@
 import time, network, ntptime, esp32, machine, socket, sys, urequests
 from machine import Pin, RTC, TouchPad, Timer, wake_reason, I2C       
-
+from struct import unpack
 ################### Functions ###################
 # Connect to Internet and print local IP (Lab 3)
 def connect(ssid, password):
@@ -28,13 +28,27 @@ def status_timer(API_KEY_READ):
 
 def notify_timer(acm_addr):
     # send notifications to IFTTT
-    val1=25
-    val2=19
-    val3=6
-    if (status=="activate" and (val1 > 0 or val2 > 0 or val3 > 0)):
+    
+    val1= i2c.readfrom_mem(acm_addr, 0x33, 1) + i2c.readfrom_mem(acm_addr, 0x32, 1)
+    val1 = unpack( '>h', val1 )[0]
+    val2= i2c.readfrom_mem(acm_addr, 0x35, 1) + i2c.readfrom_mem(acm_addr, 0x34, 1)
+    val2 = unpack( '>h', val2 )[0]
+    val3= i2c.readfrom_mem(acm_addr, 0x37, 1) + i2c.readfrom_mem(acm_addr, 0x36, 1)
+    val3 = unpack( '>h', val3 )[0]
+
+
+    val1 = (val1/256)
+    val2 = (val2/256)
+    val3 = (val3/256)
+    
+    
+    if (status=="activate" and (val1 > 1 or val2 > 1 or val3 > 2)):
+        led_red.value(1)
         url = "https://maker.ifttt.com/trigger/Motion_Detected/with/key/lxy3OOOO7S4awuW_M69tXwShf837rVsTbrqLbZDBSlL?value1="+str(val1)+"&value2="+str(val2)+"&value3="+str(val3)
         req = urequests.post(url)
         req.close()
+    else:
+        led_red.value(0)
 
 
 ############## 3.2 Software Init ###################
@@ -46,7 +60,8 @@ led_green = Pin(27, Pin.OUT) # Onboard GREEN LED is connected to IO_27
 led_red.value(0)             # should be off when init
 led_green.value(0)           # should be off when init
 
-timer_status = Timer(0)  # init hardware timer to check if activated
+timer_status = Timer(0) # init hardware timer to check if activated
+timer_notify = Timer(1) # init harware timer for ifttt notifications 
 
 i2c = I2C(1, scl=Pin(22, Pin.PULL_UP), sda=Pin(23, Pin.PULL_UP)) # create I2C bus
 
@@ -84,15 +99,30 @@ i2c.writeto_mem(acm_addr, 0x2D, b'\x08')
 #       calibrate for when device is flat and still       #                                                         
 ###########################################################
 # x and y should be 0g, z should be 1g to account for gravity
-# I calculated that z should be set to 64 base 10 (double check)
-i2c.writeto_mem(acm_addr, 0x1E, b'0x00') # x
-i2c.writeto_mem(acm_addr, 0x1F, b'0x00') # y
-i2c.writeto_mem(acm_addr, 0x20, b'0x40') # z
+# I calculated that z should be set to 64 base 10
+val1= i2c.readfrom_mem(acm_addr, 0x33, 1) + i2c.readfrom_mem(acm_addr, 0x32, 1)
+val1 = unpack( '>h', val1 )[0]
+val2= i2c.readfrom_mem(acm_addr, 0x35, 1) + i2c.readfrom_mem(acm_addr, 0x34, 1)
+val2 = unpack( '>h', val2 )[0]
+val3= i2c.readfrom_mem(acm_addr, 0x37, 1) + i2c.readfrom_mem(acm_addr, 0x36, 1)
+val3 = unpack( '>h', val3 )[0]
+
+
+
+val1 = hex(int(val1/256))
+val2 = hex(int(val2/256))
+val3 = hex(int(val3/256))
+
+i2c.writeto_mem(acm_addr, 0x1E, val1) # x
+i2c.writeto_mem(acm_addr, 0x1F, val2) # y
+i2c.writeto_mem(acm_addr, 0x20, val3) # z
+
+
 
 print("Accelerometer has been calibrated.")
 
 ############## 4 IFTTT and ThingSpeak ##############
-# 
+#        get notifications and status setup        #
 ####################################################
 API_KEY_READ = "FJVB6CAQ1GB79AKH"
 API_KEY_WRITE = "OJMQEMI54VYJ9VJX" # in IFTTT applet 1
@@ -104,12 +134,8 @@ connect('Andie', 'alevens1')
 # Hardware Timer every 30 seconds
 timer_status.init(period=30000, mode=Timer.PERIODIC, callback= lambda t: status_timer(API_KEY_READ))
 
-timer_uh = Timer(2)
-timer_uh.init(period=30000, mode=Timer.PERIODIC, callback= lambda t: print(status))
-
-timer_notify = Timer (1)
+# send notifications of accelerometer data every 10 seconds IF status == activated
 timer_notify.init(period=10000, mode=Timer.PERIODIC, callback= lambda t: notify_timer(acm_addr))
-
 
 
 
